1ee4cce2fdb48b27d8d49f7c1b3af18475b0b64b
Gabriel Rodrigues <gabrielgrs2011@hotmail.com>
2024-05-17 16:40:40 -0300

    Alterando o Program.cs - deletando class e função main

--- src/resolucaoCSharp/Program.cs
+++ src/resolucaoCSharp/Program.cs
@@ -1,26 +1,18 @@
 ﻿using System;
 using resolucaoCSharp.Models;
 
-class Program 
-{
-    static void Main(string[] args) 
-    {
-        Console.Clear();
-        PlayerStats hero = new PlayerStats();
+Console.Clear();
+PlayerStats hero = new PlayerStats();
 
-        Console.Write("Digite o número de vitórias nas partidas Ranqueadas: ");
-        hero.victories = int.Parse(Console.ReadLine());
+Console.Write("Digite o número de vitórias nas partidas Ranqueadas: ");
+hero.victories = int.Parse(Console.ReadLine());
 
-        Console.Write("Digite o número de derrotas nas partidas Ranqueadas: ");
-        hero.defeats = int.Parse(Console.ReadLine());
+Console.Write("Digite o número de derrotas nas partidas Ranqueadas: ");
+hero.defeats = int.Parse(Console.ReadLine());
 
-        int balance = hero.WinBalanceCalculator();
-        string level = hero.LevelCalculator();
+int balance = hero.WinBalanceCalculator();
+string level = hero.LevelCalculator();
 
-        Console.WriteLine($"O Herói tem saldo de {balance} está no nível de {level}");
+Console.WriteLine($"O Herói tem saldo de {balance} está no nível de {level}");
 
-        Console.ReadLine();
-    }
-
-}
-
+Console.ReadLine();
