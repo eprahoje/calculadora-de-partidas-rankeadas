a1ebf79e57cc9a8cd69b3584effe09bb836acdb3
Gabriel Rodrigues <gabrielgrs2011@hotmail.com>
2024-05-17 15:38:05 -0300

    Alterando nome da variável player para hero

--- src/resolucaoCSharp/Program.cs
+++ src/resolucaoCSharp/Program.cs
@@ -6,16 +6,16 @@ class Program
     static void Main(string[] args) 
     {
         Console.Clear();
-        PlayerStats player = new PlayerStats();
+        PlayerStats hero = new PlayerStats();
 
         Console.Write("Digite o número de vitórias nas partidas Ranqueadas: ");
-        player.victories = int.Parse(Console.ReadLine());
+        hero.victories = int.Parse(Console.ReadLine());
 
         Console.Write("Digite o número de derrotas nas partidas Ranqueadas: ");
-        player.defeats = int.Parse(Console.ReadLine());
+        hero.defeats = int.Parse(Console.ReadLine());
 
-        int balance = player.WinBalanceCalculator();
-        string level = player.LevelCalculator();
+        int balance = hero.WinBalanceCalculator();
+        string level = hero.LevelCalculator();
 
         Console.WriteLine($"O Herói tem saldo de {balance} está no nível de {level}");
 
